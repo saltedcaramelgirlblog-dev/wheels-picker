b667aab8e0f8043c4f19b166df21fbbfb8e7d6de
senouci hamid <thecrochetartisanoffice@gmail.com>
2025-08-20 20:02:43 +0100

    seo: add gatsby-plugin-sitemap and robots.txt; derive siteUrl from Netlify URL/DEPLOY_URL

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,16 +9,20 @@
  */
 const netlifyAdapter = (require("gatsby-adapter-netlify").default || require("gatsby-adapter-netlify"))
 
+// Prefer Netlify-provided URL in production, fallback to deploy URL or localhost
+const siteUrl = process.env.URL || process.env.DEPLOY_URL || "http://localhost:8000"
+
 module.exports = {
   adapter: netlifyAdapter(),
   siteMetadata: {
     title: `Wheels Picker`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl,
   },
   plugins: [
     `gatsby-plugin-image`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -40,6 +44,14 @@ module.exports = {
         // theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-robots-txt`,
+      options: {
+        host: siteUrl,
+        sitemap: `${siteUrl}/sitemap-index.xml`,
+        policy: [{ userAgent: `*`, allow: `/` }],
       },
     },
   ],
